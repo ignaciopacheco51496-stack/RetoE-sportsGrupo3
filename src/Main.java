a32c6ede2f5cd3e3c642bf5180f88bb50190cef8
Jose Miguel <josemiguel.gallego.52975@ikasle.egibide.org>
2025-11-11 12:09:57 +0100

    Primer commit de jose con el cogigo

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,80 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+
+public class Main {
+  static int numerojugadores;
+  static int numeroequipos;
+  static  Matcher matt;
+  static  int sueldo;;
+  public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, "Vamos a registrar los datos de los equipos de E-sports");
+    String equipos = JOptionPane.showInputDialog(null, "Ingrese cuantos equipos hay.(El numero debe de ser par)");
+    numeroequipos = Integer.parseInt(equipos);
+    datos_de_equipos();
+  }
+
+
+  public static void datos_de_equipos(){
+    int i=0;
+    do {
+      ++i;
+      String nombreequipo = JOptionPane.showInputDialog(null, "Ingrese el nombre del equipo");
+      String fecha= JOptionPane.showInputDialog(null, "Ingrese el fecha de creacion del equipo(dd/MM/yyyy)");
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+      LocalDate fechaCreacion = LocalDate.parse(fecha, formatter);
+      String jugadores= JOptionPane.showInputDialog(null, "Ingrese cuantos jugadores hay en el equipo");
+      Pattern pat = Pattern.compile("^[2-6]$");
+      Matcher mat = pat.matcher(jugadores);
+      if(!mat.matches()){
+
+
+        do {
+          JOptionPane.showMessageDialog((Component)null, "El numero de jugadores es incorrecto, un equipo no puede tener mas de 6 jugadores y como minimo 2 sino no podra jugar la jornada");
+          jugadores =JOptionPane.showInputDialog(null, "Ingrese el numero de jugadores del equipo, maximo son seis y minimo 2");
+          Pattern patt = Pattern.compile("^[2-6]$");
+          matt = patt.matcher(jugadores);
+        }while (!matt.matches());
+
+
+      }
+      numerojugadores = Integer.parseInt(jugadores);
+      jugadores();
+
+
+    }while(i < numeroequipos);
+  }
+
+
+  public static void jugadores(){
+    int i=0;
+    do {
+      ++i;
+      String nombrejugador= JOptionPane.showInputDialog(null, "Ingrese el nombre del jugador");
+      String apellidojugador= JOptionPane.showInputDialog(null, "Ingrese el apellido de jugador");
+      String nacionalidadjugador= JOptionPane.showInputDialog(null, "Ingrese el nacionalidad de jugador");
+      String nicknamejugador= JOptionPane.showInputDialog(null, "Ingrese el nickname");
+      String roljugador= JOptionPane.showInputDialog(null, "Ingrese el rol");
+      String salariojugador= JOptionPane.showInputDialog(null, "Ingrese el salario");
+      int salario= Integer.parseInt(salariojugador);
+      if (salario <= 1184) {
+
+
+        do {
+          String salarios= JOptionPane.showInputDialog(null, "Ingrese el sueldo.(Debe de ser mayor a 1184)");
+          sueldo = Integer.parseInt(salarios);
+        }while(sueldo < 1184);
+      }else {
+        sueldo=salario;
+      }
+      salario=sueldo;
+
+
+    }while(i < numerojugadores);
   }
 }
+
