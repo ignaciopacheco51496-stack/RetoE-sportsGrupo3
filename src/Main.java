bdd72f97ccbe6e4981286b4230fbcdbb8069f076
Jose Miguel <josemiguel.gallego.52975@ikasle.egibide.org>
2025-11-12 11:58:16 +0100

    Version futura para mas jugadores

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,16 @@ import java.util.regex.Pattern;
 
 
 public class Main {
-  static int numerojugadores;
+  static int numerojugadores ;
   static int numeroequipos;
   static  Matcher matt;
   static  int sueldo;
   static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+  static boolean a = false;
 
   public static void main(String[] args) {
     JOptionPane.showMessageDialog(null, "Vamos a registrar los datos de los equipos de E-sports");
-    boolean e = false;
-    while (e == true) {
+    while (a != true) {
       try {
         String equipos = JOptionPane.showInputDialog(null, "Ingrese cuántos equipos hay (el número debe ser par)");
         numeroequipos = Integer.parseInt(equipos);
@@ -31,19 +31,22 @@ public class Main {
         JOptionPane.showMessageDialog(null, "Por favor, ingrese un número válido.");
       }
     }
+
     datos_de_equipos();
+
+
+
 
   }
 
 
   public static void datos_de_equipos(){
     int i=0;
-    boolean e = false;
     do {
       ++i;
       String nombreequipo = JOptionPane.showInputDialog(null, "Ingrese el nombre del equipo");
       LocalDate fechaCreacion;
-      while (e==true) {
+      while (a!=true) {
         try {
           LocalDate hoy = LocalDate.now();
           String fecha = JOptionPane.showInputDialog(null, "Ingrese la fecha de creación del equipo (dd/MM/yyyy)");
@@ -83,13 +86,13 @@ public class Main {
 
   public static void jugadores(){
     int i=0;
-    boolean e = false;
+
     do {
       ++i;
       String nombrejugador= JOptionPane.showInputDialog(null, "Ingrese el nombre del jugador");
       String apellidojugador= JOptionPane.showInputDialog(null, "Ingrese el apellido de jugador");
       LocalDate fechanacimiento;
-      while (e == true) {
+      while (a != true) {
         String fecha = JOptionPane.showInputDialog(null, "Ingrese la fecha de nacimiento (dd/MM/yyyy)");
 
         try {
@@ -132,9 +135,78 @@ public class Main {
         sueldo=salario;
       }
       salario=sueldo;
+      JOptionPane.showMessageDialog(null, "El sueldo es de: " + salario);
 
 
     }while(i < numerojugadores);
+    añadirjugador();
   }
+  public static void añadirjugador(){
+    int opcion;
+    if (numerojugadores<6){
+      do {
+         opcion= JOptionPane.showConfirmDialog(
+                null,
+                "¿Quiere añadir algún jugador más?",
+                "Añadir jugador",
+                JOptionPane.YES_NO_OPTION
+        );
+
+        if(opcion == JOptionPane.YES_OPTION){
+          numerojugadores++;
+          String nombrejugador= JOptionPane.showInputDialog(null, "Ingrese el nombre del jugador");
+          String apellidojugador= JOptionPane.showInputDialog(null, "Ingrese el apellido de jugador");
+          LocalDate fechanacimiento;
+          while (a != true) {
+            String fecha = JOptionPane.showInputDialog(null, "Ingrese la fecha de nacimiento (dd/MM/yyyy)");
+
+            try {
+              fechanacimiento = LocalDate.parse(fecha.trim(), formatter);
+
+              LocalDate hoy = LocalDate.now();
+              int edad = hoy.getYear() - fechanacimiento.getYear();
+
+              if (fechanacimiento.isAfter(hoy)) {
+                JOptionPane.showMessageDialog(null, "La fecha de nacimiento no puede ser futura.");
+                continue;
+              } else if (edad < 14) {
+                JOptionPane.showMessageDialog(null, "El jugador debe tener al menos 14 años o que los cumpla este mismo año.");
+                continue;
+              } else if (edad > 60) {
+                JOptionPane.showMessageDialog(null, "El jugador no puede tener más de 60 años para jugar.");
+                continue;
+              }
+
+              break;
+
+            } catch (DateTimeParseException e) {
+              JOptionPane.showMessageDialog(null, "Formato incorrecto. Use el formato dd/MM/yyyy.");
+            }
+          }
+
+          String nacionalidadjugador= JOptionPane.showInputDialog(null, "Ingrese el nacionalidad de jugador");
+          String nicknamejugador= JOptionPane.showInputDialog(null, "Ingrese el nickname");
+          String roljugador= JOptionPane.showInputDialog(null, "Ingrese el rol");
+          String salariojugador= JOptionPane.showInputDialog(null, "Ingrese el salario");
+          int salario= Integer.parseInt(salariojugador);
+          if (salario <= 1184) {
+
+
+            do {
+              String salarios= JOptionPane.showInputDialog(null, "Ingrese el sueldo.(Debe de ser mayor a 1184)");
+              sueldo = Integer.parseInt(salarios);
+            }while(sueldo < 1184);
+          }else {
+            sueldo=salario;
+          }
+          salario=sueldo;
+          JOptionPane.showMessageDialog(null, "El sueldo es de: " + salario);
+        }
+
+      }while (opcion == JOptionPane.NO_OPTION);
+    }
+    JOptionPane.showMessageDialog(null, "Todos los datos del equipo han sido guardados, en total son "+ numerojugadores +" jugadores en el equipo");
+  }
+
 }
 
