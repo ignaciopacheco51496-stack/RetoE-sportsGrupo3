26b95d179e807c65e0f132606078f67837bfc556
IGNACIO PACHECO <ignacio.pacheco.51496@ikasle.egibide.org>
2025-11-12 11:01:19 +0100

    commit guardado 1

--- src/Main.java
+++ src/Main.java
@@ -56,19 +56,33 @@ public class Main {
     Pattern patronNombreValido = Pattern.compile("^[a-zA-Z0-9]*$]");
     String nombreEquipo;
     boolean nombreValido = false;
-    do{
+    do {
         nombreEquipo = JOptionPane.showInputDialog("Ingrese el nombre del equipo: ");
-        if(nombreEquipo==null){
+        if (nombreEquipo == null) {
             return;
         }
         Matcher matcher = patronNombreValido.matcher(nombreEquipo);
-        if(matcher.matches(){
+        if (matcher.matches()) {
             nombreValido = true;
-        }else{
+        } else {
             JOptionPane.showMessageDialog(null, "Error el nombre del equipo");
         }
+    }while(nombreValido==false);
+    int numJugadores = 0;
+    while(numJugadores < 2 || numJugadores > 6){
+        try {
+            String cantNumJugadores = JOptionPane.showInputDialog("Cuantos jugadores tiene tu equipo (min: 2, max: 6):");
+            if (cantNumJugadores == null) {
+                return;
 
-      while(nombreValido==false);
+          } else {
+                String[] jugadores = cantNumJugadores.split(",");
+                numJugadores = Integer.parseInt(jugadores[0]);
+
+
+            }
+        }
+
 
     }
     public static void calendario(){
